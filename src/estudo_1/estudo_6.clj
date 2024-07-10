abd48c3b685503c8e19fed5bfe69dc727efa2100
jaksonlima <jaksonsneider@gmail.com>
2024-07-09 23:03:33 -0300

    aula 6

---
+++ src/estudo_1/estudo_6.clj
@@ -0,0 +1,23 @@
+(ns estudo-1.estudo-6)
+
+(defn soma
+  [x y]
+  (+ x y))
+
+(class 10)
+(type 10)
+(type "10")
+(type 10.00)
+(type (/ 3 10))
+(type (* 3 (/ 10 3)))
+(type 10N)
+(type 10M)
+(type nil)
+(type [])
+(type '())
+(type println)
+(type str)
+(type +)
+(type -)
+(type *)
+(type /)
