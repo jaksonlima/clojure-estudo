026827708d7946a00e0749a711e0b2ee70c427ca
jaksonlima <jaksonsneider@gmail.com>
2024-07-12 00:29:49 -0300

    list

--- src/estudo_1/high_order.clj
+++ src/estudo_1/high_order.clj
@@ -55,35 +55,53 @@
 
 (def person-v3 {:name :test :age 27})
 
+(fn [v] (* 2 v))
 
+(#(* 2 %) 2)
 
+(map #(* 2 %) '(1 2 3))
 
+(#(* %1 %2) 2 2)
 
+(constantly 0.2)
+((constantly 0.2))
+(concat [1 2] [3 4])
+(rest [1 2 3])
+(first [1 2 3])
+(second [1 2 3])
+(take 2 [1 2 3 4])
+(drop 2 [1 2 3 4])
+(interpose :a [1 2 3])
+(partition 2 [1 2 3 4 5 6])
+(range 5)
+(repeat 3 :a)
+(take 5 (iterate inc 1))
 
+(assoc {:a 1} :b 2) ; => {:a 1, :b 2}
+(dissoc {:a 1, :b 2} :a) ; => {:b 2}
 
+(update {:a 1} :a inc) ; => {:a 2}
+(update-in {:a {:b 1}} [:a :b] inc) ; => {:a {:b 2}}
+(apply + [1 2 3])
 
+(def add5 (partial + 5))
+(add5 3) ; => 8
 
+(if true "yes" "no") ; => "yes"
+(when true "yes") ; => "yes"
+(cond
+  (= 1 1) "one"
+  (= 2 2) "two") ; => "one"
 
+(println "Hello, World!")
 
+(spit "test.txt" "Hello, World!")
+(slurp "test.txt") ; => "Hello, World!"
 
+(for [x [1 2 3 4 5]]
+  (* x x))
 
+(for [i (range 10)]
+  (println i))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(for [x (range 10) :when (even? x)] (println x))
