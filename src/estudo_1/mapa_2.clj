4d633c3b4abb467b7f57ab2e03f54048ac894b98
jaksonlima <jaksonsneider@gmail.com>
2024-07-15 14:04:15 -0300

    map parte 1

---
+++ src/estudo_1/mapa_2.clj
@@ -0,0 +1,19 @@
+(ns estudo-1.mapa-2)
+
+(defn todas-disciplinas
+  []
+  [{:nome "Estrutura de dados" :semestre 2}
+   {:nome "Algoritmos" :semestre 1}
+   {:nome "Inteligência Artificial" :semestre 3}])
+
+(todas-disciplinas)
+
+(defn nome-disiplinas-restantes
+  [disciplinas semestre-atual]
+  (filter #(>= (:semestre %1) semestre-atual) (disciplinas)))
+
+(defn nome-disiplinas-restantes
+  [disciplinas semestre-atual]
+  (filter (fn [map] (>= (:semestre map) semestre-atual)) (disciplinas)))
+
+(nome-disiplinas-restantes todas-disciplinas 1)
