92dfd92d341fb2820d697817b86dd0143b1f8142
jaksonlima <jaksonsneider@gmail.com>
2024-07-16 22:45:43 -0300

    finally

--- src/estudo_1/imc.clj
+++ src/estudo_1/imc.clj
@@ -1,12 +1,17 @@
 (ns estudo-1.imc)
 
-(defn imc
+(defn- imc-calc
   [weight height]
   (->> (* height height)
        (/ weight)))
 
-(defn imc
+(defn imc-indicator
   [weight height]
-  :low)
+  (let [imc (imc-calc weight height)]
+    (cond
+      (< imc 18.5) :low
+      (< imc 24.9) :idea
+      :else :high)))
 
-(imc 90 1.58)
+(imc-calc 90 1.58)
+(imc-indicator 1 1)
--- test/estudo_1/imc_test.clj
+++ test/estudo_1/imc_test.clj
@@ -1,6 +1,6 @@
 (ns estudo-1.imc-test
   (:require [clojure.test :refer :all])
-  (:require [estudo-1.imc :refer [imc]]))
+  (:require [estudo-1.imc :refer [imc-indicator]]))
 
 :low
 :high
@@ -8,6 +8,10 @@
 
 (deftest givenValidParams-WhenCallsImc-ThenValidInstance
   (testing "weight person imc low"
-    (is (= :low (imc 1 2)))))
+    (is (= :low (imc-indicator 1 2))))
+  (testing "weight idea"
+    (is (= :idea (imc-indicator 70 1.70))))
+  (testing "high imc"
+    (is (= :high (imc-indicator 100 1.50)))))
 
 
